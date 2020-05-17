99c8de0eb4956b22569a93254b36ab26a8591e64
Elisha Lapiz <ely-zip1@users.noreply.github.com>
2020-05-17 22:40:12 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'https://office-goldenocean.com';
-$config['base_url'] = 'http://localhost/energy-fuels/';
+$config['base_url'] = 'https://energyfuels-affiliate.com/';
 
 /*
 |--------------------------------------------------------------------------
